5a87b0effb23048c9d4e37877624284c28603db0
Simon Löschke <simon-loeschke@posteo.de>
2023-03-19 20:33:45 +0100

    änderungen

--- classes/deletes.py
+++ classes/deletes.py
@@ -1,5 +1,6 @@
 from .connector import *
-import os
+from .terminal_menu import *
+import os,time
 
 class deleteClass(verbindung):
     def __init__(self) -> None:
@@ -20,6 +21,29 @@ class deleteClass(verbindung):
                     os._exit(1)
     
     
+
+    def deleteAll(self):
+        with self.connection as connection:
+            with connection.cursor() as cursor:
+                delete = f"DELETE FROM `personen`, `laender`, `sprachen`;"
+                print(f"Achtung, Sie löschen alle Datenbankinhalte.")
+                print(f"Bitte bestätigen Sie das Löschen.")
+                confirmation = str(input("Geben Sie OK ein: >>"))
+                if confirmation == "OK":
+                    try:
+                        cursor.execute(delete)
+                        connection.commit()
+                        time.sleep(1)
+                        print(f"Es wurden alle Daten aus der Datenbank gelöscht.")
+                    except:
+                        print("Löschen fehlgeschlagen. Zurück zum Hauptmenü...")
+                        time.sleep(1)
+                        main_menu()
+                else:
+                    print("Löschen abgebrochen. Zurück zum Hauptmenu...")
+                    time.sleep(1)
+                    main_menu()
+
 
 
 
